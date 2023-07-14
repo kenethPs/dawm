760ff521d5dd0145540c76017f247321ae83dad2
kenethPs <68650722+kenethPs@users.noreply.github.com>
2023-07-13 19:46:27 -0500

    eliminacion de datos

--- proyectos/proyecto 4/js/practica.js
+++ proyectos/proyecto 4/js/practica.js
@@ -7,12 +7,15 @@
             let time_zone = data["timezone"]
             let latitude = data["latitude"]
             let longitude = data["longitude"]
+            let elevation = data["elevation"]
             let latitudeHTML = document.getElementById("latitude")
             let timezoneHTML = document.getElementById("timezone")
             let longitudeHTML = document.getElementById("longitude")
+            let elevationHTML = document.getElementById("elevation")
             timezoneHTML.textContent = time_zone
             latitudeHTML.textContent = latitude
             longitudeHTML.textContent = longitude
+            elevationHTML.textContent = elevation
         })
         .catch(console.error);
     }
